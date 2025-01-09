c878214c3667ca0f6f925ddbd59e2f6f35a91f39
Swargam Mani Mardhav <2311cs020642@mallareddyuniversity.ac.in>
2025-01-09 16:35:58 +0530

    Add files via upload

---
+++ AD ASSIGNMENTS/assign3.py
@@ -0,0 +1,28 @@
+'''Write a Python program that takes a student's marks in three subjects as input.
+•	If the average is greater than or equal to 90, print "Grade: A".
+•	If the average is between 80 and 89, print "Grade: B".
+•	If the average is between 70 and 79, print "Grade: C".
+•	Otherwise, print "Grade: Fail". '''
+def calculate_grade(marks):
+    average = sum(marks) / len(marks)
+
+    if average >= 90:
+        return "Grade: A"
+    elif 80 <= average < 90:
+        return "Grade: B"
+    elif 70 <= average < 80:
+        return "Grade: C"
+    else:
+        return "Grade: Fail"
+
+try:
+    marks = []
+    for i in range(1, 4):
+        mark = float(input(f"Enter marks for subject {i}: "))
+        marks.append(mark)
+
+    grade = calculate_grade(marks)
+    print(grade)
+
+except ValueError:
+    print("Invalid input! Please enter numeric values for marks.")
---
+++ AD ASSIGNMENTS/assign4.py
@@ -0,0 +1,23 @@
+'''Write a Python program to calculate the sum of all even numbers between 1 and a given positive integer n'''
+def sum_of_even_numbers(n):
+    sum_even = 0
+    for i in range(2, n + 1, 2):
+        sum_even += i
+    return sum_even
+
+def main():
+    while True:
+        try:
+            n = int(input("Enter a positive integer: "))
+            if n > 0:
+                break
+            else:
+                print("Please enter a positive integer.")
+        except ValueError:
+            print("Invalid input. Please enter a valid positive integer.")
+
+    sum_even = sum_of_even_numbers(n)
+    print("The sum of all even numbers between 1 and", n, "is:", sum_even)
+
+if __name__ == "__main__":
+    main()
