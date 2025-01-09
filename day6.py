e330fcec4b355088925e375ea98801176082af98
Swargam Mani Mardhav <2311cs020642@mallareddyuniversity.ac.in>
2025-01-09 07:50:31 +0000

    num

--- day6.py
+++ day6.py
@@ -5,3 +5,13 @@ type(iterate_var)
 for i in iterate_var:
     if i % 2 == 0:
         print(i)
+
+import pandas as pd
+screen_time = [1,2,5,6]
+stu_name = ["aa","bb","cc","dd"]
+id= [1,2,3,4]
+dict1 = {"screen_time":screen_time,"stu_name":stu_name,"id":id}
+print(dict1)
+df =pd.DataFrame(dict1)
+print(df) 
+df.head(5)
